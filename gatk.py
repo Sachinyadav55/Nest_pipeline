841a9d31bc7923ef9473c4f69798c91d3983e9fa
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2015-10-07 14:28:57 -0400

    Run GATK tools

--- gatk.py
+++ gatk.py
@@ -6,15 +6,41 @@ import logging
 import datetime
 import argparse
 import subprocess
-import config as cf
+import configparser
 from time import gmtime, strftime
+
+logger = logging.getLogger('Nest - GATK')
+logger.setLevel(logging.DEBUG)
+stream = logging.StreamHandler()
+stream.setLevel(logging.DEBUG)
+formats = logging.Formatter('%(asctime)s;%(name)s;%(levelname)s;%(message)s')
+stream.setFormatter(formats)
+logger.addHandler(stream)
 
 def target(samfile):
     logger.info('Running realigner target creator')
     interval = os.path.splitext(samfile)[0] + '.intervals'
-    trcr_args = [cf.java, '-jar', cf.gatk, '-T', 'RealignerTargetCreator', '-o', interval,
-            '-maxInterval', cf.maxint, '-minReads', cf.minreads, '-mismatch', cf.mismatch,
-            '-window', cf.window, '-nt', cf.threads, '-I', samfile, '-R', cf.reference ] + cf.known
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    java = config['General']['java']
+    gatk = config['GATK']['gatk']
+    maxint = config['GATK']['maxins']
+    minreads = config['GATK']['minreads']
+    mismatch = config['GATK']['mismatch']
+    window = config['GATK']['window']
+    threads = config['General']['threads']
+    reference = config['General']['reference']
+    known = list()
+    if config['GATK']['known'] == 'null':
+        known = ['-known', config['General']['dbsnp']]
+    else:
+        for lines in config['GATK']['known'].split(','):
+            known.append('-known')
+            known.append(lines)
+    
+    trcr_args = [java, '-jar', gatk, '-T', 'RealignerTargetCreator', '-o', interval,
+            '-maxInterval', maxint, '-minReads', minreads, '-mismatch', mismatch,
+            '-window', window, '-nt', threads, '-I', samfile, '-R', reference ] + known
     try:
         run_trcr = subprocess.check_call(' '.join(trcr_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         logger.info('Realginer target creator completed')
@@ -25,12 +51,27 @@ def target(samfile):
 
 
 def realigner(samfile, interval):
+
     logger.info('Running indel realigner')
     bamfile = os.path.splitext(samfile)[0] + '_IR.bam'
-    inre_args = [cf.java, '-jar', cf.gatk, '-T', 'IndelRealigner', '--targetIntervals', interval,
-            '-o', bamfile, '-model', cf.model, '-LOD', cf.lod, '-entropy', cf.entropy,
-            '--maxConsensuses', cf.maxcon, '-maxIsize', cf.maxins, '-maxPosMove', cf.maxmoves, 
-            '-greedy', cf.greedy, '-maxReads', cf.maxreads, '-maxInMemory', cf.maxinmem, '-I', samfile, '-R', cf.reference]
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    java = config['General']['java']
+    gatk = config['GATK']['gatk']
+    model = config['GATK']['model']
+    lod = config['GATK']['lod']
+    entropy = config['GATK']['entropy']
+    maxins = config['Bowtie']['maxins']
+    maxmoves = config['GATK']['maxmoves']
+    maxcon = config['GATK']['maxcon']
+    reference = config['General']['reference']
+    greedy = config['GATK']['greedy']
+    maxreads = config['GATK']['maxreads']
+    maxinmem = config['GATK']['maxinmem']
+    inre_args = [java, '-jar', gatk, '-T', 'IndelRealigner', '--targetIntervals', interval,
+            '-o', bamfile, '-model', model, '-LOD', lod, '-entropy', entropy,
+            '--maxConsensuses', maxcon, '-maxIsize', maxins, '-maxPosMove', maxmoves, 
+            '-greedy', greedy, '-maxReads', maxreads, '-maxInMemory', maxinmem, '-I', samfile, '-R', reference]
     try:
         run_inre = subprocess.check_call(' '.join(inre_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         logger.info('Indel realignment completed')
@@ -40,12 +81,35 @@ def realigner(samfile, interval):
     return(bamfile)
 
 def baserecal(samfile):
+
     logger.info('Running base recalibrator')
     table = os.path.splitext(samfile)[0] + '.table'
-    bsrc_args = [cf.java, '-jar', cf.gatk, '-T', 'BaseRecalibrator', '-R', cf.reference,
-            '-I', samfile, '-o', table, '-ics', cf.ics, '-maxCycle', cf.maxcyc, '-mcs', 
-            cf.mcs, '-bqsrBAQGOP', cf.bqsrgpen, '-ddq', cf.ddq, '-idq', cf.idq, '-nct', cf.threads] + cf.covariates + cf.knownsites
-    print (' '.join(bsrc_args))
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    java = config['General']['java']
+    reference = config['General']['reference']
+    gatk = config['GATK']['gatk']
+    ics =  config['GATK']['ics']
+    maxcyc = config['GATK']['maxcyc']
+    mcs = config['GATK']['mcs']
+    bqsrgpen = config['GATK']['bqsrpen']
+    ddq = config['GATK']['ddq']
+    idq = config['GATK']['idq']
+    threads = config['General']['threads']
+    covariates = list()
+    for covs in config['GATK']['covariates'].split(','):
+        covariates.append('-cov')
+        covariates.append(covs)
+    knownsites = list()
+    if config['GATK']['knownsites'] == 'None':
+        knownsites = ['-knownSites',config['General']['dbsnp']]
+    else:
+        for sites in config['GATK']['knownsites'].split(','):
+            knownsites.append('-knownSites')
+            knownsites.append(sites)
+    bsrc_args = [java, '-jar', gatk, '-T', 'BaseRecalibrator', '-R', reference,
+            '-I', samfile, '-o', table, '-ics', ics, '-maxCycle', maxcyc, '-mcs', 
+            mcs, '-bqsrBAQGOP', bqsrgpen, '-ddq', ddq, '-idq', idq, '-nct', threads] + covariates + knownsites
     try:
         run_bsrc = subprocess.check_call(' '.join(bsrc_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         logger.info('Base recalibration completed')
@@ -55,10 +119,17 @@ def baserecal(samfile):
     return(table)
 
 def printreads(samfile, table):
+
     logger.info('Running print reads')
     bamfile = os.path.splitext(samfile)[0] + '_BQ.bam'
-    prre_args = [cf.java, '-jar', cf.gatk, '-T', 'PrintReads', '-R', cf.reference, '-I',
-            samfile, '-o', bamfile, '-BQSR', table, '-nct', cf.threads]
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    java = config['General']['java']
+    gatk = config['GATK']['gatk']
+    reference = config['General']['reference']
+    threads = config['General']['threads']
+    prre_args = [java, '-jar', gatk, '-T', 'PrintReads', '-R', reference, '-I',
+            samfile, '-o', bamfile, '-BQSR', table, '-nct', threads]
     try:
         run_prre = subprocess.check_call(' '.join(prre_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         logger.info('Print reads completed')
@@ -68,9 +139,20 @@ def printreads(samfile, table):
     return(bamfile)
 
 def haplocaller(samfile):
+
     logger.info('Running haplotype caller')
-    vcffile = os.path.splitext(samfile)[0] + '.vcf'
-    hpcl_args = [cf.java, '-jar', cf.gatk, '-T', 'HaplotypeCaller', '-R', cf.reference, '-I', samfile, '--dbsnp', cf.dbsnp, '-stand_call_conf', cf.calconf, '-stand_emit_conf', cf.emitconf, '-o', vcffile, '-nct', cf.threads, '-gt_mode', cf.gtmode]
+    vcffile = os.path.splitext(samfile)[0] + '.g.vcf'
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    java = config['General']['java']
+    gatk = config['GATK']['gatk']
+    reference = config['General']['reference']
+    dbsnp = config['General']['dbsnp']
+    calconf = config['GATK']['calconf']
+    emitconf = config['GATK']['emitconf']
+    threads = config['General']['threads']
+    gtmode = config['GATK']['gtmode']
+    hpcl_args = [java, '-jar', gatk, '-T', 'HaplotypeCaller', '-R', reference, '-I', samfile, '--dbsnp', dbsnp,'--emitRefConfidence', 'GVCF', '-stand_call_conf', calconf, '-stand_emit_conf', emitconf, '-o', vcffile, '-nct', threads, '-gt_mode', gtmode]
     try:
         run_hpcl = subprocess.check_call(' '.join(hpcl_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         logger.info('Haplotype caller completed')
@@ -79,19 +161,32 @@ def haplocaller(samfile):
         sys.exit()
     return(vcffile)
 
+def joint_genotyper(gvcf_list, outdir):
+
+    logger.info('Running joint genotyping')
+    vcffile = outdir + '/joint_calls.vcf'
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    java = config['General']['java']
+    gatk = config['GATK']['gatk']
+    reference = config['General']['reference']
+    variants = [' --variant '+ val for val in gvcf_list]
+    jg_args = [java, '-jar', gatk, '-T', 'GenotypeGVCFs', '-R', reference, '-o', vcffile] + variants
+    print (jg_args)
+    try:
+        run_jg = subprocess.check_call(' '.join(jg_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        logger.info('Joint Genptyper completed')
+    except subprocess.CalledProcessError as ret:
+        logger.info('GATk failed with return code: {0}'.format(ret.returncode))
+        sys.exit()
+    return(vcffile)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run GATK')
     parser.add_argument('-b', type=str, dest='bamfile', help='Bam file path')
     args = parser.parse_args()
-    logger = logging.getLogger('Nest - GATK')
-    logger.setLevel(logging.DEBUG)
-    stream = logging.StreamHandler()
-    stream.setLevel(logging.DEBUG)
-    formats = logging.Formatter('%(asctime)s;%(name)s;%(levelname)s;%(message)s')
-    stream.setFormatter(formats)
-    logger.addHandler(stream)
-    #interval = target(args.bamfile)
-    #bam = realigner(args.bamfile, interval)
+    interval = target(args.bamfile)
+    bam = realigner(args.bamfile, interval)
     table = baserecal(args.bamfile)
     bam = printreads(args.bamfile, table)
     vcffile = haplocaller(bam)
