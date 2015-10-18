74fc5c8d7d9dd1486ae1be960ccc9905f69018f0
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2015-10-18 15:42:24 -0400

    Bowtie presets added

--- bowtie.py
+++ bowtie.py
@@ -25,10 +25,6 @@ def bowtie(read1,read2,outdir,base):
     bowtie = config['Bowtie']['bowtie']
     index = config['Bowtie']['index']
     qual_form = '--phred33'
-    mode = config['Bowtie']['mode']
-    mismatch = config['Bowtie']['mismatch']
-    seedlen = config['Bowtie']['seedlen']
-    interval = config['Bowtie']['interval']
     ambfunc = config['Bowtie']['ambfunc']
     dpad = config['Bowtie']['dpad']
     gbar = config['Bowtie']['gbar']
@@ -37,31 +33,43 @@ def bowtie(read1,read2,outdir,base):
     ambpen = config['Bowtie']['ambpen']
     gappen = config['Bowtie']['gappen']
     refgappen = config['Bowtie']['refgappen']
-    maplim = config['Bowtie']['maplim']
     minins = config['Bowtie']['minins']
     maxins = config['Bowtie']['maxins']
     orient = config['Bowtie']['orient']
-    seedex = config['Bowtie']['seedex']
-    reseed = config['Bowtie']['reseed']
     threads = config['General']['threads']
-    if read2 == None:
-        bowtie_args = [ bowtie, '-x', index, '-U', read1, '-S',outfile, qual_form,
-                    mode, '-N', mismatch, '-L', seedlen, '-i', interval, '--n-ceil', 
-                    ambfunc, '--dpad', dpad, '--gbar', gbar, '--ma', matbonus, 
-                    '--mp', penrange, '--np', ambpen, '--rdg', gappen, '--rfg', 
-                    refgappen, '-k', maplim, '-D', seedex, '-R', reseed, '-p', threads]
-    else :
-        bowtie_args = [ bowtie, '-x', index, '-1', read1, '-2', read2, '-S',outfile,
-                        qual_form, mode, '-N', mismatch, '-L', seedlen, '-i', interval,
-                        '--n-ceil', ambfunc, '--dpad', dpad, '--gbar', gbar, '--ma', 
-                        matbonus, '--mp', penrange, '--np', ambpen, '--rdg', gappen, 
-                        '--rfg', refgappen, '-k', maplim, '-D', seedex, '-R', reseed, 
-                        '-I', minins, '-X', maxins, orient, '-p', threads]
     try:
-        run_bowtie = subprocess.check_call(' '.join(bowtie_args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        preset = config['Bowtie']['preset']
+        bowtie_args = [ bowtie, '-x', index, '-S',outfile, qual_form,
+            preset, '--n-ceil', ambfunc, '--dpad', dpad, '--gbar', gbar, 
+            '--ma', matbonus, '--mp', penrange, '--np', ambpen, '--rdg', 
+            gappen, '--rfg', refgappen, '-p', threads]
+    except KeyError:
+        seedex = config['Bowtie']['seedex']
+        reseed = config['Bowtie']['reseed']
+        seedlen = config['Bowtie']['seedlen']
+        mode = config['Bowtie']['mode']
+        interval = config['Bowtie']['interval']
+        mismatch = config['Bowtie']['mismatch']
+        bowtie_args = [ bowtie, '-x', index, '-S',outfile, qual_form,
+            mode, '-N', mismatch, '-L', seedlen, '-i', interval, '--n-ceil', 
+            ambfunc, '--dpad', dpad, '--gbar', gbar, '--ma', matbonus,
+            '--mp', penrange, '--np', ambpen, '--rdg', gappen, '--rfg', 
+            refgappen, '-D', seedex, '-R', reseed, '-p', threads]
+    try:
+        maplim = config['Bowtie']['maplim']
+        bowtie_args += ['-k', maplim]
+    except KeyError:
+        maplim = None
+    if read2 == None:
+        bowtie_args += ['-U', read1, '--quiet']
+    else :
+        bowtie_args += ['-1', read1, '-2', read2, '--quiet']
+    try:
+        run_bowtie = subprocess.check_call(' '.join(bowtie_args), shell=True)
         logger.info('Read alignment completed successfully')
     except subprocess.CalledProcessError as ret:
-        logger.info('Bowtiefailed with return code: '+ str(ret.returncode))
+        logger.info('Bowtie failed with return code: {0}'.format(ret.returncode))
+        logger.info('Bowtie command: {0}'.format(ret.cmd))
         sys.exit()
     return(outfile)
  
